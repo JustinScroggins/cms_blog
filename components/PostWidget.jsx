d2a23ba9fd1123af801b599083b6f5e17471e308
Justin Scroggins <justinscroggins@outlook.com>
2021-11-13 21:57:30 -0500

    Add getRecentPosts and getSimilarPosts

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import Link from 'next/link';
 
-const PostWidget = ({}) => {
+import { getRecentPosts, getSimilarPosts } from '../services';
+
+const PostWidget = ({ categories, slug }) => {
+    const [relatedPosts, setRelatedPosts] = useState([]);
+
+    useEffect(() => {
+        if(slug) {
+            getSimilarPosts(category, slug)
+                .then((result) => setRelatedPosts(result))
+        } else {
+            getRecentPosts()
+            .then((result) => setRelatedPosts(result)) 
+        }
+    }, [slug])
+
+    console.log(relatedPosts)
+
     return (
-        <div>
-            PostWidget
+        <div className="bg-white shadow-lg rounded-lg p-8 mb-8 text-black">
+            <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+                {slug ? 'Related Posts' : 'Recent Posts'}
+            </h3>
+            {relatedPosts.map((post) => (
+                <div key={post.title} className="min-width:max-content mb-4">
+                    <div className="w-16 min-width:max-content">
+                        <img 
+                            alt={post.title}
+                            height="60px"
+                            width="60px"
+                            className="align-middle rounded-full"
+                            src={post.featuredImage.url}
+                        />
+                        <div className="flex-grow ml-4">
+                            <p className="text-gray-500 font-xs ">
+                                {moment(post.createdAt).format('MMM DD, YYYY')}
+                            </p>
+                            <Link href={`/post/${post.slug}`} className="text-md" key={post.title}>{post.title}</Link>
+
+                        </div>
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
